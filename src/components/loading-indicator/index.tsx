8050746bddec494b73409a64bd667e20a9eb7fed
vu.duong <vu.duong@schwarm.com>
2024-12-07 14:50:11 +0700

    editing #1

---
+++ src/components/loading-indicator/index.tsx
@@ -0,0 +1,11 @@
+export const LoadingIndicator = () => {
+  return (
+    <div
+      id="loading-spinner"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+    >
+      <div className="relative">
+      </div>
+    </div>
+  );
+};
